29123ad47ed904d7c02ddf71c50aa41bd9b289d2
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-02-14 12:01:11 +0300

    zephyr: plugins/ua_config_default.c: Limit buffer sizes.
    For Zephyr, limit default buffer/message size to 8K, which is the minimum
    supported by OPC.
    
    Perhaps, this could be handled by making a custom config for Zephyr, instead
    of using default, but follow the easy way here.

--- plugins/ua_config_default.c
+++ plugins/ua_config_default.c
@@ -41,9 +41,9 @@ UA_DURATIONRANGE(UA_Duration min, UA_Duration max) {
 
 const UA_ConnectionConfig UA_ConnectionConfig_default = {
     0, /* .protocolVersion */
-    65535, /* .sendBufferSize, 64k per chunk */
-    65535, /* .recvBufferSize, 64k per chunk */
-    0, /* .maxMessageSize, 0 -> unlimited */
+    8192, /* .sendBufferSize, 64k per chunk */
+    8192, /* .recvBufferSize, 64k per chunk */
+    8192, /* .maxMessageSize, 0 -> unlimited */
     0 /* .maxChunkCount, 0 -> unlimited */
 };
 
